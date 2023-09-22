ef7f16f49a873aaa95571728a395b6a48b0eb7a8
brycewoodland <bwoodlan@byui.edu>
2023-09-21 22:33:46 -0600

    text added

--- Lesson2/scripts/getdate.js
+++ Lesson2/scripts/getdate.js
@@ -1,3 +1,3 @@
 let today1 = new Date();
 document.querySelector("#currentyear").textContent=today1.getFullYear()
-document.querySelector("#lastmodified").textContent=document.lastModified
+document.querySelector("#lastModified").textContent=document.lastModified
