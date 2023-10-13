42478d228d4c4fb688334323bcad48c158c53c75
brycewoodland <bwoodlan@byui.edu>
2023-10-13 11:08:00 -0600

    dark mode finished

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,21 @@
+const darkmode = document.querySelector('#dark-mode');
+const bodyelt = document.querySelector('body');
+
+darkmode.addEventListener('click', () => {
+    if (darkmode.textContent == 'DARK') {
+        document.documentElement.style.setProperty('--text-color', '#ECF0F1');
+        document.documentElement.style.setProperty('--background-color', '#171A21');
+        document.documentElement.style.setProperty('--paragraph-color', '#ECF0F1');
+        document.documentElement.style.setProperty('--header-background', '#264348');
+        document.documentElement.style.setProperty('--hover-color', 'yellow');
+        darkmode.textContent = 'LIGHT';
+    }
+    else{
+        document.documentElement.style.setProperty('--background-color', '#ECF0F1');
+        document.documentElement.style.setProperty('--text-color', '#171A21');
+        document.documentElement.style.setProperty('--paragraph-color', '#171A21');
+        document.documentElement.style.setProperty('--header-background', '#096B72');
+        document.documentElement.style.setProperty('--hover-color', 'yellow');
+        darkmode.textContent = 'DARK';
+    }
+})
