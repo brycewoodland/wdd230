89e366153c559861d8ac1a0ecd4d8d93a213344b
brycewoodland <bwoodlan@byui.edu>
2023-10-16 11:26:57 -0600

    chamber home page content added

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -12,7 +12,7 @@ darkmode.addEventListener('click', () => {
     }
     else{
         document.documentElement.style.setProperty('--background-color', '#ECF0F1');
-        document.documentElement.style.setProperty('--text-color', '#171A21');
+        document.documentElement.style.setProperty('--text-color', '#ECF0F1');
         document.documentElement.style.setProperty('--paragraph-color', '#171A21');
         document.documentElement.style.setProperty('--header-background', '#096B72');
         document.documentElement.style.setProperty('--hover-color', 'yellow');
