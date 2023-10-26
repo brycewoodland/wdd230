8f4eff89973e75a50855a2255c47aedbddc9b919
brycewoodland <bwoodlan@byui.edu>
2023-10-26 15:25:33 -0600

    page visit added

--- scripts/pagevisit.js
+++ scripts/pagevisit.js
@@ -1,14 +1,19 @@
-const visitsDisplay = document.querySelector('.visits');
+// Create Key
+const VISITS_KEY = 'site-visits';
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
+function getSiteVisits() {
+   // Check to see if the key exists in local storage
+   let currentValue = localStorage.getItem(VISITS_KEY);
+   // If the key doesn't exist, initialize the key to 1
+   let siteVisits = 1;
+   
+   if (currentValue != null) {
+      siteVisits = parseInt(currentValue) + 1;
+      // If the key does exist, add 1 to the current value
+   }
+   // Save the new value for current visits
+   localStorage.setItem(VISITS_KEY, `${siteVisits}`);
+   return siteVisits;
 }
-else {
-    visitsDisplay.textContent = `This is your first time visiting!`;
-}
-
-numVisits++;
-
-localStorage.setItem("numVisists-ls", numVisits);
+// Update the html page with the current visits
+document.querySelector('#visitcount').textContent = `${getSiteVisits()}`;
