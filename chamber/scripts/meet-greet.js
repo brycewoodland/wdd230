ca253ef7b5c63c8d314cd7b19b1c78f8c0dc43cc
brycewoodland <bwoodlan@byui.edu>
2023-11-17 19:47:14 -0700

    join

---
+++ chamber/scripts/meet-greet.js
@@ -0,0 +1,7 @@
+let meetgreet = new Date();
+let dayOfWeek = meetgreet.getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3)
+{
+    let meetgreet = document.querySelector("#meetgreet");
+}
