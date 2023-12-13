99d46145d99e128cba3ab78443a6a5b185b79bf3
brycewoodland <bwoodlan@byui.edu>
2023-12-13 15:34:33 -0700

    nutrition added

--- bountiful/scripts/orderconfirmation.js
+++ bountiful/scripts/orderconfirmation.js
@@ -1,3 +1,5 @@
+const currentDate = new Date().toLocaleDateString();
+
 let url = new URL(window.location);
 let params = url.searchParams;
 
@@ -13,6 +15,7 @@ async function getFruitNameById(fruitId) {
     return fruit ? fruit.name : 'Unknown Fruit';
 }
 
+document.querySelector('#orderDate').textContent = currentDate;
 document.querySelector('#yourname').textContent = params.get("firstName");
 document.querySelector('#email').textContent = params.get("email");
 document.querySelector('#phone').textContent = params.get("phoneNumber");
@@ -21,3 +24,38 @@ document.querySelector('#fruit2').textContent = getFruitNameById(params.get("fru
 document.querySelector('#fruit3').textContent = getFruitNameById(params.get("fruit3"));
 document.querySelector('#specialinstructions').textContent = params.get("specialInstructions");
 
+async function fetchNutritionalInfo(fruitId) {
+    const response = await fetch("./data/fruit.json");
+    const fruitData = await response.json();
+    
+    const fruit = fruitData.find(f => f.id === parseInt(fruitId));
+    return fruit ? fruit.nutritions : null;
+}
+
+async function updatehealthInfo() {
+    const fruit1Info = await fetchNutritionalInfo(params.get("fruit1"));
+    const fruit2Info = await fetchNutritionalInfo(params.get("fruit2"));
+    const fruit3Info = await fetchNutritionalInfo(params.get("fruit3"));
+
+    if (fruit1Info && fruit2Info && fruit3Info) {
+        const round = (value) => parseFloat(value).toFixed(2);
+
+        const totalNutrition = {
+            carbs: round(fruit1Info.carbohydrates + fruit2Info.carbohydrates + fruit3Info.carbohydrates),
+            protein: round(fruit1Info.protein + fruit2Info.protein + fruit3Info.protein),
+            fat: round(fruit1Info.fat + fruit2Info.fat + fruit3Info.fat),
+            sugar: round(fruit1Info.sugar + fruit2Info.sugar + fruit3Info.sugar),
+            calories: fruit1Info.calories + fruit2Info.calories + fruit3Info.calories
+        };
+
+    document.querySelector('#carbs').textContent = totalNutrition.carbs;
+    document.querySelector('#protein').textContent = totalNutrition.protein;
+    document.querySelector('#fat').textContent = totalNutrition.fat;
+    document.querySelector('#sugar').textContent = totalNutrition.sugar;
+    document.querySelector('#calories').textContent = totalNutrition.calories;
+    } else {
+        console.error("failed to fetch nutrional information for one or more fruits.");
+    }
+}
+
+updatehealthInfo();
